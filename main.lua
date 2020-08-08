cfba67db26a446dd962a40b1f20422011de37d55
Ícaro Emannuel <emannuelalmeida@gmail.com>
2020-08-08 15:34:39 -0300

    Map importing and validation feature added. For it only reads one hardcoded map. A map manager resource for level progressing is expected, but this shall be made only after the physics and basic graphics part.

--- main.lua
+++ main.lua
@@ -1,22 +1,24 @@
+require(".util")
 require(".map")
 require(".media")
 require(".input")
+require(".hero")
 
 --Start of the game
 function love.load()
-    map = loadMap()
+    map = loadMap("maps\\map1.txt")
 end
- 
+
 -- Proccess the inputs and the events. Actually don't know what to with the dt. Am I rusty or something like it?
 function love.update(dt)
     --For now only the simple logic will be added. Later I'll add more stuff as screens.
     deltaX, deltaY = ProcessPositionDelta()
 end
  
--- Draw the map or the event 
+-- Draw the map or the event
 function love.draw()
     if (map.victory()) then
-        --Victory screen here
+        --Victory screen here, then wait a little bit and then wait to see if there's another map.
     else
         --Normal drawing of map here. Take the other screen to get it
         love.graphics.print(deltaX, 30, 200)
---
+++ util.lua
@@ -0,0 +1,13 @@
+function Split(str, sep)
+    
+    if sep == nil then
+        sep = "%s"
+    end
+
+    local t = {}
+    for str in string.gmatch(str, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+
+    return t
+end
