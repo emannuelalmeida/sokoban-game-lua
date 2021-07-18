6b9a081b1c8aa06d75140a131b0e44a9c450af02
Ícaro Emannuel <emannuelalmeida@gmail.com>
2021-07-18 15:25:56 -0300

    Added camera and clipping

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Another remake of sokoban, but this time written in Lua with Löve
 
 Next features for 3.0:
 
-- Camera view to move inside bigger maps.
+- Camera view to move inside bigger maps. - DONE
+- Applying clipping function - DONE
 - Hero has a direction. Also a better sprite for our hero.
 - Sounds.
 - Packaging.
--- screen.lua
+++ screen.lua
@@ -1,4 +1,4 @@
-local scaleFactor, spriteSize, shiftFactorX, shiftFactorY = 1, 0, 0, 0
+local shiftFactorX, shiftFactorY, sizeXOnScreen, sizeYOnScreen = 0, 0, 0, 0
 local spriteSize = 35
 local map = {}
 local color = {1, 0, 1, 1}
@@ -6,34 +6,79 @@ local color = {1, 0, 1, 1}
 function AdjustMapToScreen(mapObj)
 
     map = mapObj
-    local sizeX = map.sizeX * spriteSize
-    local sizeY = map.sizeY * spriteSize
+    sizeXOnScreen = map.sizeX * spriteSize
+    sizeYOnScreen = map.sizeY * spriteSize
 
-    if (sizeX < WinWidth) then
-        shiftFactorX = math.abs(sizeX - WinWidth)/2
+    if (sizeXOnScreen < WinWidth) then
+        shiftFactorX = math.abs(sizeXOnScreen - WinWidth)/2
     else
         shiftFactorX = 0
     end
 
-    if (sizeY < WinHeight) then
-        shiftFactorY = math.abs(sizeY - WinHeight)/2
+    if (sizeYOnScreen < WinHeight) then
+        shiftFactorY = math.abs(sizeYOnScreen - WinHeight)/2
     else
         shiftFactorY = 0
     end
 
 end
 
+local function checkAndAdjustShiftFactors()
+    if (sizeXOnScreen > WinWidth) then
+        if ((map.hero.x ) * spriteSize + shiftFactorX  > WinWidth/2
+            and sizeXOnScreen + shiftFactorX >= WinWidth ) then
+                shiftFactorX = shiftFactorX - spriteSize
+            end
+        
+        if ((map.hero.x) * spriteSize + shiftFactorX < WinWidth/2
+            and shiftFactorX < 0 ) then
+                shiftFactorX = shiftFactorX + spriteSize
+            end
+    end
+
+    if (sizeYOnScreen > WinHeight) then
+        if ((map.hero.y ) * spriteSize + shiftFactorY  > WinHeight/2
+            and sizeYOnScreen + shiftFactorY >= WinHeight ) then
+                shiftFactorY = shiftFactorY - spriteSize
+            end
+        
+        if ((map.hero.y) * spriteSize + shiftFactorY < WinHeight/2
+            and shiftFactorY < 0 ) then
+                shiftFactorY = shiftFactorY + spriteSize
+            end
+    end
+
+end
+
+local function isCellDrawable(x, y)
+    local vpXStart = -shiftFactorX
+    local vpXEnd = WinWidth - shiftFactorX
+
+    local vpYStart = -shiftFactorY
+    local vpYEnd = WinHeight - shiftFactorY
+
+    if (x * spriteSize >= vpXStart and x * spriteSize <= vpXEnd
+        and y * spriteSize >= vpYStart and y * spriteSize <= vpYEnd) then
+            return true
+        else
+            return false
+        end
+end
+
 local function drawGameScreen()
+    
+    checkAndAdjustShiftFactors()
+    
     for i=1, map.sizeY do
         for j=1, map.sizeX do
             local cell = map.grid[i][j]
-            if cell ~= 'H' and cell ~= ' ' then
-                love.graphics.draw(Images[cell], (j-1)*spriteSize+shiftFactorX, (i-1)*spriteSize+shiftFactorY, 0, scaleFactor, scaleFactor)
+            if cell ~= 'H' and cell ~= ' ' and isCellDrawable(j, i) then
+                love.graphics.draw(Images[cell], (j-1)*spriteSize+shiftFactorX, (i-1)*spriteSize+shiftFactorY, 0)
             end
         end
     end
 
-    love.graphics.draw(Images["hero"], (map.hero.x-1)*spriteSize+shiftFactorX, (map.hero.y-1)*spriteSize+shiftFactorY, 0, scaleFactor, scaleFactor)
+    love.graphics.draw(Images["hero"], (map.hero.x-1)*spriteSize+shiftFactorX, (map.hero.y-1)*spriteSize+shiftFactorY, 0)
 end
 
 local function drawStartScreen()
