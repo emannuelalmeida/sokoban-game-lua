32f1de59cd4f1ebe0eef0193e03f1c37ea888495
Ícaro Emannuel <emannuelalmeida@gmail.com>
2021-08-16 21:21:53 -0300

    Sounds were inserted to play in some events. These sounds may change later for better ones

--- media.lua
+++ media.lua
@@ -1,18 +1,29 @@
 local function loadAllImages()
     local images = {}
 
-    images["B"] = love.graphics.newImage("media/box.jpg")
-    images["X"] = love.graphics.newImage("media/boxOnSlot.jpg")
-    images["W"] = love.graphics.newImage("media/wall.jpg")
-    images["S"] = love.graphics.newImage("media/slot.jpg")
-    images["hero"] = love.graphics.newImage("media/hero.jpg")
-    images["start"] = love.graphics.newImage("media/start.jpg")
-    images["victory"] = love.graphics.newImage("media/victory.jpg")
-    images["highscore"] = love.graphics.newImage("media/highscore.jpg")
-    images["playerName"] = love.graphics.newImage("media/player.jpg")
+    images["B"] = love.graphics.newImage("media/images/box.jpg")
+    images["X"] = love.graphics.newImage("media/images/boxOnSlot.jpg")
+    images["W"] = love.graphics.newImage("media/images/wall.jpg")
+    images["S"] = love.graphics.newImage("media/images/slot.jpg")
+    images["hero"] = love.graphics.newImage("media/images/hero.jpg")
+    images["start"] = love.graphics.newImage("media/images/start.jpg")
+    images["victory"] = love.graphics.newImage("media/images/victory.jpg")
+    images["highscore"] = love.graphics.newImage("media/images/highscore.jpg")
+    images["playerName"] = love.graphics.newImage("media/images/player.jpg")
 
     return images
 
 end
 
+local function loadAllSounds()
+    local sounds = {}
+
+    sounds["menu"] = love.audio.newSource("media/sounds/menu.mp3", "static")
+    sounds["confirma"] = love.audio.newSource("media/sounds/confirma_menu.mp3", "static")
+    sounds["caixa"] = love.audio.newSource("media/sounds/caixa.ogg", "static")
+
+    return sounds
+end
+
 Images = loadAllImages()
+Sounds = loadAllSounds()
